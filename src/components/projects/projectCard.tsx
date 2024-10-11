edaf484fd36e4aa2a27f12b8cfed414f85e2961f
Jose Arpaia <jose@etyalab.com>
2024-10-10 21:56:25 -0500

    update projectCard component

--- src/components/projects/projectCard.tsx
+++ src/components/projects/projectCard.tsx
@@ -13,37 +13,35 @@ export default function ProjectCard({
   return (
     <div
       className={[
-        'relative flex min-h-[60vh] w-[80vw] flex-col justify-around rounded-lg bg-teal-800 text-teal-200 transition-all duration-300 md:h-full md:w-[30vw]',
+        'w-[85vw] rounded-lg bg-teal-700 transition-all duration-300 ease-in-out hover:shadow-md md:h-full md:w-[30vw]',
         url && 'cursor-pointer hover:opacity-80',
       ]
         .filter(Boolean)
         .join(' ')}
       onClick={() => url && window.open(url, '_blank')}
     >
-      <div className="m-0 -mt-4 p-0">
-        {image && (
-          <div className="sticky inset-0 h-full w-full rounded-full bg-teal-800">
-            <img
-              src={image}
-              alt={title}
-              className="h-52 w-full rounded-xl object-cover object-left-top"
-            />
-            <h3 className="mt-2 px-4 text-lg font-bold">{title}</h3>
-          </div>
-        )}
-      </div>
-      <div className="text-ellipsis px-4 text-teal-200/80">{description}</div>
-      <div className="flex flex-wrap gap-2 p-4">
-        {projectTags?.map((tag) => (
-          <Chip key={tag} url={tags[tag].url}>
-            <div className="flex items-center gap-1 text-sm">
-              {tags[tag].icon && (
-                <img src={tags[tag].icon} alt={tags[tag].title} className="h-4 w-4" />
-              )}
-              {tags[tag].title}
-            </div>
-          </Chip>
-        ))}
+      {image && (
+        <img
+          src={image}
+          alt={title}
+          className="sticky inset-0 m-0 h-52 w-full rounded-lg bg-teal-800 object-cover object-left-top p-0"
+        />
+      )}
+      <div className="px-2 py-1">
+        <h3 className="my-2 px-4 text-lg font-bold">{title}</h3>
+        <div className="text-ellipsis px-4 text-teal-200/80">{description}</div>
+        <div className="flex flex-wrap gap-2 p-4">
+          {projectTags?.map((tag) => (
+            <Chip key={tag} url={tags[tag].url}>
+              <div className="flex items-center gap-1 text-sm">
+                {tags[tag].icon && (
+                  <img src={tags[tag].icon} alt={tags[tag].title} className="h-4 w-4" />
+                )}
+                {tags[tag].title}
+              </div>
+            </Chip>
+          ))}
+        </div>
       </div>
     </div>
   );
